c5bd7c52d8727024d597c39c3402063dc1227bee
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-06-21 17:58:20 +0900

    0621

---
+++ HELLO_PYTHON/day02/myoop02.py
@@ -0,0 +1,39 @@
+class xi:
+    def __init__(self):
+        self.money = 1000
+    
+    def steal(self, smoney):
+        self.money += smoney
+        
+class putin:
+    def __init__(self):
+        self.nuclear = 5000
+    def alzheimer(self):
+        self.nuclear -= 1
+
+class JungEun:
+    def __init__(self):
+        self.missile = 10000
+    def ssorau(self):
+        self.missile -= 100
+        
+class Sungwoo(xi, putin, JungEun):
+    def __init__(self):
+        xi.__init__(self)
+        putin.__init__(self)
+        JungEun.__init__(self)
+        
+        
+s = Sungwoo()
+print("xi:",s.money)
+print("putin:",s.nuclear)  
+print("JungEun:",s.missile)  
+
+s.steal(300)
+s.alzheimer()
+s.ssorau()
+
+print("xi steal:",s.money)
+print("putin alzheimer:",s.nuclear)  
+print("JungEun ssorau:",s.missile)  
+       
