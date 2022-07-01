873e0bb749cd5ad376ccf5d5de90622a13e043fc
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-07-01 20:01:58 +0900

    0701 homework

--- HELLO_WEB/src/main/java/kr/co/aiai/dao/Mydelete.java
+++ HELLO_WEB/src/main/java/kr/co/aiai/dao/Mydelete.java
@@ -2,25 +2,24 @@ package kr.co.aiai.dao;
 import java.sql.*;
 
 
-public class Myupdate {
+public class Mydelete {
 
 	public static void main(String[] args) throws Exception {
 
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3305/python", "root", "python");
 		Statement stmt = conn.createStatement();
-		String sql = "UPDATE emp SET e_name=1, sex=1, addr=1 WHERE e_id = 2";
-				
+		String sql = "DELETE from emp where e_id = 3";
 		
 	
-		stmt = conn.prepareStatement(sql);
-		int result = stmt.executeUpdate(sql);
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		int result = pstmt.executeUpdate();
 		// 3. 결과 확인
 		System.out.println("결과 : " + result);
 
- 
+
 		
-		stmt.close();
+		pstmt.close();
 		conn.close();
 	}
 }
