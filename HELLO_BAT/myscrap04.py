e13dbf8a61407519088a4ddcf7cee048fe2a5045
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-07-14 12:27:03 +0900

    0714

---
+++ HELLO_BAT/myscrap04.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+from daostock import DaoStock
+
+
+ds = DaoStock()
+
+now = datetime.datetime.now()
+ymd = now.strftime("%Y%m%d.%H%M")
+
+url = "https://vip.mk.co.kr/newSt/rate/item_all.php"
+
+html = requests.get(url)
+html.encoding='EUC-KR'
+
+
+soup = BeautifulSoup(html.text, 'html.parser')
+tds = soup.find_all("td","st2")
+
+for td in tds:
+    s_name = td.text
+    s_code = td.a['title']
+    price = td.parent.find_all("td")[1].text.replace(",","")
+    cnt = ds.insert(s_code, ymd, s_name, price)
+    print("cnt",cnt)  
+
