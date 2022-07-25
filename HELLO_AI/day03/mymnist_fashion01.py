73dc8012d3359d8694b3011583f459db14c704b6
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-07-25 13:53:01 +0900

    0725

--- HELLO_AI/day03/mymnist_fashion01.py
+++ HELLO_AI/day03/mymnist_fashion01.py
@@ -1,15 +1,12 @@
 import tensorflow as tf
 import numpy as np
-from keras import models
-from keras import layers
-from keras.utils import to_categorical
 import cv2
 
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-for idx, l in enumerate(train_labels):
+for idx, i in enumerate(train_labels):
     label = train_labels[idx]
     print(f'train/{label}/{idx}.jpg')
-    cv2.imwrite(f'train/{label}/{idx}.jpg',train_labels[idx])
+    cv2.imwrite(f'train/{label}/{idx}.jpg',train_images[idx])
