493da57f7b0faf21bd363969ce2be8e7974af6de
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-08-17 17:41:14 +0900

    0817

--- HELLO_AI/day17_test/myflast01.py
+++ HELLO_AI/day17_test/myflast01.py
@@ -7,10 +7,9 @@ from flask.json import jsonify
 app = Flask(__name__,static_url_path='')
 
 @app.route('/')
-@app.route('/omok')
 
 def omok():
-    return render_template('omok3_19.html')
+    return render_template('test09.html')
 
 
 if __name__ == '__main__':
