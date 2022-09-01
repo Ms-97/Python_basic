40fad2d1ecf3c9d06046c501e1961ac27c2786cc
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-09-01 13:52:52 +0900

    0831

---
+++ HELLO_AI2/day01/myfft01.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+
+t = np.arange(0, 3, 1 / 100)
+
+signal = 0.6 * np.sin(2 * np.pi  * t)
+
+print(t)
+print(signal)
+
+fft = np.fft.fft(signal) / len(signal)  
+ 
+fft_magnitude = abs(fft)
+
+plt.subplot(2,1,1)
+plt.plot(t,signal)
+plt.grid()
+ 
+plt.subplot(2,1,2)
+plt.stem(fft_magnitude)
+plt.ylim(0,2.5)
+plt.grid()
+plt.show()
